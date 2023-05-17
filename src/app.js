df1358ee183c762e46bdcab6ba69c0ce69f27303
Carlin Mitchell <carlinmi@umn.edu>
2023-05-17 10:44:51 -0500

    “add font and basic styles”

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 //############### COMPONENT IMPORTS #################
 import Footer from "./components/Footer/Footer";
+import Content from "./components/Content";
 
 //################# STYLE IMPORTS ###################
 import "./style.css";
 
+document.body.appendChild(Content());
 document.body.appendChild(Footer());
