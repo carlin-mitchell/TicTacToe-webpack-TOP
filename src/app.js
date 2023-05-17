b9bf76ce19d73b4a19e0bfed2cbdbefc6cc96a09
Carlin Mitchell <carlinmi@umn.edu>
2023-05-17 09:16:08 -0500

    “style test”

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 //############### COMPONENT IMPORTS #################
 import Footer from "./components/Footer/Footer";
 
+//################# STYLE IMPORTS ###################
+import "./style.css";
+
 document.body.appendChild(Footer());
